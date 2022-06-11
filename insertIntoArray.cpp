850bb29c796a60db37774376475c2f77179c60b4
namrata77 <namratathakur077@gmail.com>
2022-06-11 11:50:44 +0530

    djhj

--- insertIntoArray.cpp
+++ insertIntoArray.cpp
@@ -25,14 +25,19 @@ int main()
 {
     vector<int> arr={2,4,5,7,8,0};
     int num = 6;
-    int i = arr.size()-1;
-    while(arr[i] > num){
-        arr[i+1] = arr[i];
-        i--;
-    }
-    arr[i+1] = num;
+    for( int i=arr.size()-2; i>=0; i--){
+        if(arr[i] > num){
+            arr[i+1] = arr[i];
+            arr[i] = 0;}
+        
 
-    
+        else if (arr[i] < num){
+            arr[i+1] = num; 
+            break;
+        }
+        
+
+    }
 
     for(int i=0; i<arr.size(); i++)
     cout<<arr[i]<<" ";
