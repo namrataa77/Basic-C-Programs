4f884e1d818962330b6d90006ed3866f450bc749
namrata77 <namratathakur077@gmail.com>
2022-07-15 17:29:52 +0530

    circular linkedlist

--- circularLinkedList.cpp
+++ circularLinkedList.cpp
@@ -4,7 +4,7 @@ using namespace std;
 struct node{
     int data;
     struct node* next;
-}; struct node *head;
+}; struct node *head, *last;
 
 void display(node* p){
     p = head;
@@ -16,7 +16,7 @@ void display(node* p){
 }
 
 void create(vector<int> A){
-    struct node *t, *last;
+    struct node *t;
     head = new node;
     head->data = A[0];
     head->next=head;
@@ -29,14 +29,34 @@ void create(vector<int> A){
         last->next = t;
         last = t;
     }
+}
 
+void insert(int x, int pos){
+    struct node* p = head;
+    struct node* t;
+    if(pos == 1){
+        t=new node;
+        t->data = x;
+        t->next = head;
+        last->next = t;
+        head = t;
+    }
+    else {
+    for(int i = 1; i<pos-1; i++){
+        p = p->next;
+    }
+    t = new node;
+    t->data = x;
+    t->next = p->next;
+    p->next = t;}
 
-
+    // cout<<endl<<head->data;
 }
 
 int main(){
- vector<int> A = {1,2,3,4,5};
+ vector<int> A = {2,3,4,5};
  create(A);
+ insert(1,1);
  display(head);
 
 }
