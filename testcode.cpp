1e6d380b27ed2da181c4f58b9e53aad016504e71
namrata77 <namratathakur077@gmail.com>
2022-07-18 11:56:04 +0530

    first contest

--- testcode.cpp
+++ testcode.cpp
@@ -1,86 +1,61 @@
-/* C++ program to implement basic stack
-operations */
-#include <bits/stdc++.h>
+#include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-#define MAX 1000
-
-class Stack {
-	int top;
-
-public:
-	int a[MAX]; // Maximum size of Stack
-
-	Stack() { top = -1; }
-	bool push(int x);
-	int pop();
-	int peek();
-	bool isEmpty();
-};
-
-bool Stack::push(int x)
-{
-	if (top >= (MAX - 1)) {
-		cout << "Stack Overflow";
-		return false;
+int main() {
+	int p;
+	cin>>p;
+	int A[p][3];
+    int X,Y,Z;
+    int res = 0, total = 0, time = 0;
+    int breaks = 0, Q = 0, rem = 0;
+	for(int i=0; i<p;i++){
+	    for(int j=0; j<3; j++){
+	        cin>>A[i][j];
+	    }
 	}
-	else {
-		a[++top] = x;
-		cout << x << " pushed into stack\n";
-		return true;
-	}
-}
-
-int Stack::pop()
-{
-	if (top < 0) {
-		cout << "Stack Underflow";
-		return 0;
-	}
-	else {
-		int x = a[top--];
-		return x;
-	}
-}
-int Stack::peek()
-{
-	if (top < 0) {
-		cout << "Stack is Empty";
-		return 0;
-	}
-	else {
-		int x = a[top];
-		return x;
-	}
-}
-
-bool Stack::isEmpty()
-{
-	return (top < 0);
-}
-
-// Driver program to test above functions
-int main()
-{
-	class Stack s;
-	s.push(10);
-	s.push(20);
-	s.push(30);
-	cout << s.pop() << " Popped from stack\n";
 	
-	//print top element of stack after poping
-	cout << "Top element is : " << s.peek() << endl;
-	
-	//print all elements in stack :
-	cout<<"Elements present in stack : ";
-	while(!s.isEmpty())
-	{
-		// print top element in stack
-		cout<<s.peek()<<" ";
-		// remove top element in stack
-		s.pop();
-	}
+	for(int i=0; i<p;i++){
+	    for(int j=0; j<3; j++){
+	        X = A[i][0];
+            Y = A[i][1];
+            Z = A[i][2];
 
+            if(j==2){
+                Q = X/3;
+                if(X <= 3){
+                    res = X*Y;
+                    cout<<res<<endl;
+                }
+                else{
+                    if(X%3 == 0){
+                        
+                        breaks = Z * (Q-1);
+                        
+                        time = Q * Y * 3;
+                        
+
+                        total = breaks + time;
+                        cout<<total<<endl;
+                    }
+                    else{
+                        breaks = Q * Z;
+                        rem = X%3;
+                        time = (Q*3*Y)+(rem*Y);
+
+                        total = breaks + time;
+                        cout<<total<<endl;
+                    }
+                }
+            
+            }
+            
+            
+	        
+	    }
+        
+        
+	}
 	return 0;
 }
