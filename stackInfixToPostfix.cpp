cf2dbebc50678d65ee463c67726d71fc59927d7e
namrata77 <namratathakur077@gmail.com>
2022-07-19 13:09:04 +0530

    sdfhgdh

--- stackInfixToPostfix.cpp
+++ stackInfixToPostfix.cpp
@@ -50,17 +50,28 @@ int Stack::prec(char x){
     else if(x == '*' || x == '/') return 2;
     return 0;
 }
-char* infix2Postfix(char * infix){
+char* infix2Postfix(char* infix){
     struct Stack st;
-    char* postfix = new char[strlen(infix)+1];
+    int len = strlen(infix);
+    char* postfix = new char[len+1];
     int i = 0, j = 0;
-    while(infix[i] = '\0'){
-        if (st.isOperand(infix[i])) postfix[j++] = infix[i++];
+    
+    while(infix[i] != '\0'){
+        
+        if (st.isOperand(infix[i])) {
+            postfix[j++] = infix[i++]; 
+              
+            }
         else{
-            if(st.prec(infix[i])>st.prec(st.top->data)) 
+            
+            if(st.top == NULL) st.push(infix[i++]);
+            else {
+                if(st.prec(infix[i])>st.prec(st.top->data)) 
                 st.push(infix[i++]);
             else 
                 postfix[j++] = st.pop();
+            }
+            
         }
     }
     while(st.top != NULL){
@@ -72,7 +83,6 @@ char* infix2Postfix(char * infix){
 int main()
 {
     char* exp = "a+b*c-d/e";
-    int i = 0;
     cout<<infix2Postfix(exp);
    
     return 0;
