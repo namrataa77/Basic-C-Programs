1cbe472d29303b345e609c764de8f4a40873af10
namrata77 <namratathakur077@gmail.com>
2022-08-05 22:19:50 +0530

    dhgdsh

--- TreeGraphs.cpp/dhgfdhs.cpp
+++ TreeGraphs.cpp/dhgfdhs.cpp
@@ -2,21 +2,13 @@
 #include<stdio.h>
 using namespace std;
 
-// int func(int m){
-//     if(m>100) return m-10;
-//     return func(func(m+11));
-// }
-
 int main()
 {
-    void *p;
-    int a[4] = {1,2,3,4};
-    p = &a[3];
-    int *ptr = &a[2];
-    int n = (int*)p - ptr;
-    cout<<n;
+    char str[20] = "Hello";
+    char *const p = str;
+    *p = 'J';
+    printf("%s", str);
     return 0;
 }
-
 
 
